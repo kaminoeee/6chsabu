b50f22ee98e2c7b7bfad62ab6ff0b56178d29dd7
kaminoeee <kaminoeee@gmail.com>
2026-08-31 05:17:44 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'; // 必要なら空ファイルでも OK
+import './globals.css'
 
 export const metadata = {
   title: '匿名掲示板',
   description: 'GitHub + Vercel で 1 クリックデプロイできる匿名掲示板',
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="ja">
       <body>{children}</body>
     </html>
-  );
+  )
 }
